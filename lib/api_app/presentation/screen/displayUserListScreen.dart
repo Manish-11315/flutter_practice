e00db6042b3069150ab80872a5b6f491b8a755ec
Manish-11315 <manishkapri1325@gmail.com>
2026-09-01 23:45:35 +0530

    added network check while loading the data

--- lib/api_app/presentation/screen/displayUserListScreen.dart
+++ lib/api_app/presentation/screen/displayUserListScreen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_project_practice/api_app/presentation/bloc/userBloc.dart
 import 'package:flutter_project_practice/api_app/presentation/bloc/userbloc_events.dart';
 import 'package:flutter_project_practice/api_app/presentation/bloc/userbloc_states.dart';
 import 'package:flutter_project_practice/api_app/presentation/screen/searchUserScreen.dart';
+import 'package:flutter_project_practice/connectivity_app/presentation/bloc/connectivity_bloc.dart';
+import 'package:flutter_project_practice/connectivity_app/presentation/bloc/connectivity_states.dart';
 
 import '../widget/usersdatalist_widget.dart';
 
@@ -41,40 +43,66 @@ class Displayuserlistscreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
-              child: BlocConsumer<Userbloc, UserblocStates>(
+              child: BlocConsumer<ConnectivityBloc, ConnectivityStates>(
                 builder: (context, state) {
-                  if (state is loadingUserBloc) {
-                    return Center(child: CircularProgressIndicator());
-                  } else if (state is listAllUserdataUserBloc) {
-                    final datacount = state.userentity.length;
-                    return Column(
-                      children: [
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: datacount,
-                            itemBuilder: (BuildContext context, int index) {
-                              final datauser = state.userentity[index];
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: UsersdatalistWidget(
-                                  userId: datauser.userId,
-                                  id: datauser.id,
-                                  title: datauser.title,
-                                  iscompleted: datauser.completed,
+                  if (state is connectedState) {
+                    return BlocConsumer<Userbloc, UserblocStates>(
+                      builder: (context, state) {
+                        if (state is loadingUserBloc) {
+                          return Center(child: CircularProgressIndicator());
+                        } else if (state is listAllUserdataUserBloc) {
+                          final datacount = state.userentity.length;
+                          return Column(
+                            children: [
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount: datacount,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                        final datauser =
+                                            state.userentity[index];
+                                        return Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: UsersdatalistWidget(
+                                            userId: datauser.userId,
+                                            id: datauser.id,
+                                            title: datauser.title,
+                                            iscompleted: datauser.completed,
+                                          ),
+                                        );
+                                      },
                                 ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
+                              ),
+                            ],
+                          );
+                        } else if (state is errorUserBloc) {
+                          return Text("An error occurred : ${state.errormsg}");
+                        }
+                        return Container(child: Text("New data is not coming"));
+                      },
+                      listener: (context, state) {},
                     );
-                  } else if (state is errorUserBloc) {
-                    return Text("An error occurred : ${state.errormsg}");
+                  } else if (state is notConnectedState) {
+                    return Center(child: Icon(Icons.wifi_off));
                   }
-                  return Container(child: Text("New data is not coming"));
+                  return Center(child: Text("Loading network status"));
                 },
                 listener: (context, state) {
-
+                  if (state is connectedState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Connected"),
+                        backgroundColor: Colors.greenAccent,
+                      ),
+                    );
+                  } else if (state is notConnectedState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Not Connected"),
+                        backgroundColor: Colors.redAccent,
+                      ),
+                    );
+                  }
                 },
               ),
             ),
