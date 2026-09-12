d3193f1faf3421721e006523ad89780fc6b7143c
Manish-11315 <manishkapri1325@gmail.com>
2026-09-12 23:10:19 +0530

    Added interceptor class foundation

---
+++ lib/interceptors_practice/inceptor_class.dart
@@ -0,0 +1,7 @@
+import 'package:dio/dio.dart';
+
+class InceptorClass extends Interceptors{
+  static final InceptorClass interceptorclass = InceptorClass._internal();
+
+  InceptorClass._internal();
+}
