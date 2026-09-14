7759016ed2f7fecfe49a733daf5f2df0034a7d7e
Manish-11315 <manishkapri1325@gmail.com>
2026-09-14 22:39:13 +0530

    added interceptor class

--- lib/interceptors_practice/inceptor_class.dart
+++ lib/interceptors_practice/inceptor_class.dart
@@ -9,23 +9,36 @@ class DioInterceptorTest extends StatefulWidget {
 }
 
 class _DioInterceptorTestState extends State<DioInterceptorTest> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(dioceptor.printDio() as String),
-      ),
+        body: Container(
+            child: Text("this is data : ${callmethod()}")
+        )
     );
   }
+
+  Future<String> callmethod() async {
+    return await dioceptor.printDio();
+ }
 }
 
-
-class dioceptor{
+class dioceptor {
   static final Dio dioobj = Dio();
 
-  static Future<Response<dynamic>> printDio()async{
-    final result = await dioobj.get("https://jsonplaceholder.typicode.com/todos");
-    print(result.data);
-    return result.data;
+  static Future<String> printDio() async {
+    try {
+      final result = await dioobj.get(
+        "https://jsonplaceholder.typicode.com/todos",
+      );
+      print(
+        " \n this is the response of the API : \n ========================================================= \n${result.data}",
+      );
+      final datalist = result.data;
+      return datalist["title"];
+    } catch (err) {
+      return err.toString();
+    }
   }
 }
