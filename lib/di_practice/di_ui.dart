476ec13d0c4531a1d320070a3173527b687b63bc
Manish-11315 <manishkapri1325@gmail.com>
2026-09-24 16:50:14 +0530

    New commit

--- lib/di_practice/di_ui.dart
+++ lib/di_practice/di_ui.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_practice/di_practice/di_object.dart';
 import 'package:flutter_project_practice/di_practice/test_class.dart';
+
 class DiUi extends StatefulWidget {
   const DiUi({super.key});
 
@@ -13,9 +14,14 @@ class _DiUiState extends State<DiUi> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: MaterialButton(onPressed: (){
-          final int? id = getInstance<TestClass>().id;
-        }),
+        child: MaterialButton(
+          onPressed: () {
+            final int? id = getInstance<TestClass>().id;
+            final String? name = getInstance<TestClass>().name;
+            debugPrint("ID : $id ,  \t name : $name ");
+          },
+          child: Text("Send Data"),
+        ),
       ),
     );
   }
